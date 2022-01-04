ed7db5ca6d06b59b3fa49c99849b07427b417faa
craig <coreymcmurray14@gmail.com>
2022-01-04 16:41:30 -0700

    Modified more files

--- Asset/mainMenu.cpp
+++ Asset/mainMenu.cpp
@@ -10,14 +10,22 @@ MainMenu::~MainMenu() {
 }
 
 void MainMenu::DisplayMenu() {
-    while(true) {
-        std::string choice;
-        std::cin >> choice;
-        selection = std::stoi(choice);
-        // std::stringstream(choice) >> choice;
+    std::cout << "   === Main Menu ===   " << std::endl;
+    std::cout << "   1. Add Asset   " << std::endl;
+    std::cout << "   2. Exit   " << std::endl;
 
-        std::cout << selection << std::endl;
-        break;
+
+    std::string choice;
+    std::cin >> choice;
+    selection = std::stoi(choice);
+        
+    switch(selection) {
+        case 1:
+            std::cout << "Make an asset" << std::endl;
+            break;
+        case 2:
+            return;
     }
-
+    DisplayMenu();
+    
 }
